965e7a45b928cc6d7bb4f35bc71102270f7716f4
Rafael Flores <21326800+ElFitaFlores@users.noreply.github.com>
2025-05-18 08:59:44 +0200

    Add fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Created by `pipx` on 2025-04-22 15:34:35
+set PATH $PATH /home/fita/.local/bin
+set fish_greeting
+zoxide init fish | source
