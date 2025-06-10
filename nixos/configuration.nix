e586c2ee3698a8ffa0620f8384a7393ae7b7af96
Rafael Flores <21326800+ElFitaFlores@users.noreply.github.com>
2025-06-10 17:58:42 +0200

    Separate into nixos packages module

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
+      /etc/nixos/packages.nix
     ];
 
   # Bootloader.
@@ -78,43 +79,7 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    neovim
-    neofetch
-    ghostty
-    brave
-    wofi
-    stow
-    waybar
-    hyprlock
-    hyprpaper
-    killall
-    playerctl
-    spotify
-    localsend
-    hyprcursor
-    rose-pine-hyprcursor
-    fish
-    zoxide
-    protonmail-desktop
-    ranger # File manager CLI
-    kdePackages.okular # PDF viewer
-    nextcloud-client
-    obsidian
-    lutris
-    lazygit
-  ];
-
-  programs.hyprland.enable = true;
-  services.gnome.gnome-keyring.enable = true;
-  services.displayManager.ly = {
-    enable = true;
-  };
-  security.pam.services.ly.enableGnomeKeyring = true;
-  programs._1password-gui.enable = true;
-  programs.git.enable = true;
-  programs.steam.enable = true;
+  #programs.steam.enable = true;
   #programs.zoxide = {
   #  enable = true;
     #enableBashIntegration= true;
---
+++ nixos/packages.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim
+    neofetch
+    ghostty
+    brave
+    wofi
+    stow
+    waybar
+    hyprlock
+    hyprpaper
+    killall
+    playerctl
+    spotify
+    localsend
+    hyprcursor
+    rose-pine-hyprcursor
+    fish
+    zoxide
+    protonmail-desktop
+    ranger # File manager CLI
+    kdePackages.okular # PDF viewer
+    nextcloud-client
+    obsidian
+    lutris
+    lazygit
+    bat
+    eza
+  ];
+
+  programs.hyprland.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  services.displayManager.ly = {
+    enable = true;
+  };
+  security.pam.services.ly.enableGnomeKeyring = true;
+  programs._1password-gui.enable = true;
+  programs.git.enable = true;
+  programs.steam.enable = true;
+ }
