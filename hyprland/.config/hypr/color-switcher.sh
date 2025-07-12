edf093238ed667f5700e6bcc1c02826bf8ba9c33
Rafael Flores <21326800+ElFitaFlores@users.noreply.github.com>
2025-07-12 21:17:11 +0200

    Add themes to waybar

--- hyprland/.config/hypr/color-switcher.sh
+++ hyprland/.config/hypr/color-switcher.sh
@@ -14,6 +14,23 @@ update_theme_mode() {
     echo "$next_mode" > "$mode_file"
 }
 
+set_waybar_style() {
+    waybar_styles="$HOME/.config/waybar/style"
+    waybar_style_link="$HOME/.config/waybar/theme.css"
+    style_file="$next_mode.css"
+
+    pkill waybar
+
+    if [ -n "$style_file" ]; then
+        ln -sf "$style_file" "$waybar_style_link"
+    else
+        echo "Style file not found for $next_mode theme."
+    fi
+
+    waybar &
+}
+
 update_theme_mode
+set_waybar_style
 
 exit 0
